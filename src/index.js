3cab609501f438b552642fcc4f9e3328214b0b81
Paulo <pvinics@gmail.com>
2024-11-20 14:30:05 -0300

    Rotas para obter, atualizar e excluir um pet

--- src/index.js
+++ src/index.js
@@ -59,6 +59,105 @@ app.post("/pets", (req, res) => {
     }
 });
 
+// Obter um pet pelo seu ID
+// GET /pets/:id
+app.get("/pets/:id", (req, res) => {
+    try {
+        // 1 entrada
+        const { id } = req.params;
+
+        // 2 processamento
+        const pet = pets.find(item => item.id === id);
+        if(!pet) {
+            return res.status(404).send({
+                ok: false,
+                mensagem: "Pet não foi encontrado"
+            });
+        }
+
+        // 3 saida
+        res.status(200).send({
+            ok: true,
+            mensagem: "Pet obtido com sucesso",
+            dados: pet
+        });
+    } catch (error) {
+        res.status(500).send({
+            ok: false,
+            mensagem: error.toString()
+        });
+    }
+});
+
+// Atualizar um pet existente
+// PUT /pets/:id
+app.put("/pets/:id", (req, res) => {
+    try {
+        // 1 entrada
+        const { id } = req.params;
+        const { nome, raca, idade, nomeTutor } = req.body;
+
+        // 2 processamento
+        const pet = pets.find(item => item.id === id);
+        if(!pet) {
+            return res.status(404).send({
+                ok: false,
+                mensagem: "Pet não foi encontrado"
+            });
+        }
+
+        pet.nome = nome;
+        pet.raca = raca;
+        pet.idade = idade;
+        pet.nomeTutor = nomeTutor;
+
+        // 3 saida
+        res.status(200).send({
+            ok: true,
+            mensagem: "Pet atualizado com sucesso",
+            dados: pets
+        });
+    } catch (error) {
+        res.status(500).send({
+            ok: false,
+            mensagem: error.toString()
+        });
+    }
+});
+
+// Excluir um pet existente
+// DELETE /pets/:id
+app.delete("/pets/:id", (req, res) => {
+    try {
+        // 1 entrada
+        const { id } = req.params;
+
+        // 2 processamento
+        const petIndex = pets.findIndex(item => item.id === id);
+        if(petIndex < 0) {
+            return res.status(404).send({
+                ok: false,
+                mensagem: "Pet não foi encontrado"
+            });
+        }
+
+        pets.splice(petIndex, 1);
+
+        // 3 saida
+        res.status(200).send({
+            ok: true,
+            mensagem: "Pet excluido com sucesso",
+            dados: pets
+        });
+
+    } catch (error) {
+        res.status(500).send({
+            ok: false,
+            mensagem: error.toString()
+        });
+    }
+});
+
 const porta = process.env.PORT;
 app.listen(porta, () => {
     console.log(`O servidor está executando na porta ${porta}`);
