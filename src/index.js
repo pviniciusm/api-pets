b217f25aeb75dc11d59ceb41694e878f94904339
Paulo <pvinics@gmail.com>
2024-11-20 14:02:12 -0300

    Rotas para listagem e criacao de pets

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import * as dotenv from 'dotenv';
+import { pets } from './pets.js';
+import { randomUUID } from 'crypto';
 
 dotenv.config();
 
@@ -8,7 +10,54 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Definir rotas
+// Listar todos os Pets
+// GET /pets
+app.get("/pets", (req, res) => {
+    try {
+        res.status(200).send({
+            ok: true,
+            mensagem: "Pets listados com sucesso",
+            dados: pets
+        });
+    } catch (error) {
+        res.status(500).send({
+            ok: false,
+            mensagem: error.toString()
+        });
+    }
+});
+
+// Criar um novo pet
+// POST /pets
+app.post("/pets", (req, res) => {
+    try {
+        // 1 entrada
+        const { nome, idade, raca, nomeTutor } = req.body;
+        
+        // 2 processamento
+        const novoPet = {
+            id: randomUUID(),
+            nome,
+            idade,
+            raca,
+            nomeTutor
+        };
+
+        pets.push(novoPet);
+
+        // 3 saida
+        res.status(201).send({
+            ok: true,
+            mensagem: "Pet criado com sucesso",
+            dados: pets
+        });
+    } catch(error) {
+        res.status(500).send({
+            ok: false,
+            mensagem: error.toString()
+        });
+    }
+});
 
 const porta = process.env.PORT;
 app.listen(porta, () => {
